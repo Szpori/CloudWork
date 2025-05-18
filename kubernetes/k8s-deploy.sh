2fc9477b8660f2b326da7f9b8e5dae7b2e6663c5
Szpori <usunesezawsze@gmail.com>
2025-05-18 10:52:36 +0000

    adding Automate Docker Image Deployment

--- kubernetes/k8s-deploy.sh
+++ kubernetes/k8s-deploy.sh
@@ -1,24 +1,30 @@
-#!/bin/bash
-
 NAMESPACE=$1
-
 if [ -z "$NAMESPACE" ]; then
-  NAMESPACE="default"
+  NAMESPACE="local-dev"
 fi
+
+# change to true in case of need deleting resources first
+DELETE_BEFORE_DEPLOY="false"
 
 echo "Deploying service into k8s namespace ${NAMESPACE}"
 
-# Ensure namespace exists
-kubectl create namespace "$NAMESPACE" || echo "Namespace $NAMESPACE already exists."
+kubectl create namespace "$NAMESPACE" 2>/dev/null || true
 
-# Apply deployment and service manifests
-kubectl -n "$NAMESPACE" apply -f deployment.yaml
-kubectl -n "$NAMESPACE" apply -f service.yaml
+# optionally delete the existing resources
+if [ "$DELETE_BEFORE_DEPLOY" == "true" ]; then
+  echo "Deleting the existing resources..."
+  kubectl delete service my-node-app-service -n="$NAMESPACE"
+  kubectl delete deployment my-node-app -n="$NAMESPACE"
+fi
 
-# Restart service pods
-kubectl rollout restart deployment my-node-app --namespace="$NAMESPACE"
+# Apply the YAML files located in the root directory
+kubectl apply -f ../deployment.yaml -n="$NAMESPACE"
+kubectl apply -f ../service.yaml -n="$NAMESPACE"
 
-# Ensure deployment status
-kubectl rollout status deployment my-node-app --namespace="$NAMESPACE"
+# Restart the deployment
+kubectl rollout restart deployment my-node-app --namespace=$NAMESPACE
+
+# Check rollout status
+kubectl rollout status deployment my-node-app --namespace=$NAMESPACE
 
 echo "Deployment to Kubernetes in namespace ${NAMESPACE} completed successfully."
